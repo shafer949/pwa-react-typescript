246bd30d9a8a103eb8603dbdf593bd2ee050ed72
shafer949 <shafer949@gmail.com>
2020-10-13 10:35:21 -0500

    add functionality for quiz app

--- src/About.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About: React.FC = () => (
-  <h1>About this PWA</h1>
-);
-
-export default About;
--- src/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home: React.FC = () => (
-  <h1>Welcome!</h1>
-);
-
-export default Home;
